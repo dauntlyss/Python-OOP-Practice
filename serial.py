2714aef0378617b4f0e30f62a9fa79cd902a98c8
Alyssa Harris <amh302918@gmail.com>
2021-12-21 16:50:04 -0500

    Finished adding methods to apps

--- serial.py
+++ serial.py
@@ -20,3 +20,20 @@ class SerialGenerator:
     100
     """
 
+    def __init__(self, start=100):
+        """Creates the serial number to start, with default start being 100"""
+        self.start = self.serial = start
+        
+
+    def generate(self):
+        """Increments the serial number by one"""
+        self.serial += 1
+        return self.serial
+
+    def __repr__(self):
+        """Shows string representation of object"""
+        return f"<SerialGenerator where start is {self.start} and the next number is {self.serial + 1}>"
+
+    def reset(self):
+        """Resets serials number to start"""
+        self.serial = self.start
